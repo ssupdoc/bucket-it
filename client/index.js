c8f670fea73644d2c8ddad36cf1953351a8679c1
ssupdoc <sriramemailsyou@gmail.com>
2020-08-01 22:55:52 +1200

    Fix: refactors map code

--- client/index.js
+++ client/index.js
@@ -2,9 +2,14 @@ window.onload = () => {
     fetchNearbyPlaces()
 }
 
+const AUCKLAND_LOCATION = { lat: -36.8483, lng: 174.7625 }
+
+/**
+ * Render map based on current location
+ */
 function renderMap() {
     map = new google.maps.Map(document.getElementById("googleMap"), {
-        center: { lat: -36.8483, lng: 174.7625},
+        center: AUCKLAND_LOCATION,
         zoom: 6
     });
     infoWindow = new google.maps.InfoWindow();
@@ -18,14 +23,8 @@ function renderMap() {
                     lng: position.coords.longitude
                 };
 
-                var marker = new google.maps.Marker({
-                    position: pos,
-                    map: map,
-                    title: 'You are here'
-                  });
-                
-                map.setCenter(pos);
-                map.setZoom(10);
+                addMarker(map, pos, 'You are here')
+                setMapView(map, pos, 10)
             },
             () => {
                 handleLocationError(true, infoWindow, map.getCenter());
@@ -35,20 +34,52 @@ function renderMap() {
         // Browser doesn't support Geolocation
         handleLocationError(false, infoWindow, map.getCenter());
     }
+}
 
-    function handleLocationError(
-        browserHasGeolocation,
-        infoWindow,
-        pos
-    ) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(
-            browserHasGeolocation
-                ? "Error: The Geolocation service failed."
-                : "Error: Your browser doesn't support geolocation."
-        );
-        infoWindow.open(map);
-    }
+/**
+ * Handles location error based on error
+ * @param {*} browserHasGeolocation flag for geolocation support with browser
+ * @param {*} infoWindow info window in map
+ * @param {*} pos the lat long to display the info window
+ */
+function handleLocationError(
+    browserHasGeolocation,
+    infoWindow,
+    pos
+) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(
+        browserHasGeolocation
+            ? "Error: The Geolocation service failed."
+            : "Error: Your browser doesn't support geolocation."
+    );
+    infoWindow.open(map);
+}
+
+/**
+ * Sets the view of the map
+ * @param {*} map the map object
+ * @param {*} pos the lat long to set view for
+ * @param {*} zoom the zoom on the target
+ */
+function setMapView(map, pos, zoom) {
+    map.setCenter(pos);
+    map.setZoom(zoom);
+}
+
+/**
+ * Adds marker on the map
+ * @param {*} map the map object
+ * @param {*} pos the lat long to set view for
+ * @param {*} title the title of the marker
+ */
+function addMarker(map, pos, title) {
+    let marker = new google.maps.Marker({
+        position: pos,
+        map: map,
+        title: title
+    });
+    return marker
 }
 
 /**
